c98b1ad6e2928ea2958fe1095465942ace5e880c
Floozutter <floozutter@gmail.com>
2019-11-19 20:23:46 -0800

    Use RustAudio synth library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-// test building with cursive using cursive/examples/hello_world.rs
+// test build with cursive using cursive/examples/hello_world.rs
 
 use cursive::views::TextView;
 use cursive::Cursive;
+
+mod synthtest;
 
 fn main() {
     let mut siv = Cursive::default();
@@ -9,10 +11,16 @@ fn main() {
     // We can quit by pressing `q`
     siv.add_global_callback('q', Cursive::quit);
 
+    // Test synth by pressing `s`
+    siv.add_global_callback('s', |_c| {
+        synthtest::run().unwrap();
+    });
+
     // Add a simple view
     siv.add_layer(TextView::new(
         "Hello, world!\n\
-         Press q to quit the application.",
+         Press q to quit the application.\n\
+	 Press s to test the synth.",
     ));
 
     // Run the event loop
