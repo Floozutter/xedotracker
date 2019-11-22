efaf3bc03beb5c42b9fb6519676d35ecebe72696
Floozutter <floozutter@gmail.com>
2019-11-21 19:49:15 -0800

    Switch from PortAudio and synth to only cpal for audio I/O

---
+++ src/beeptest.rs
@@ -0,0 +1,65 @@
+// modified from cpal/examples/beep.rs and cpal docs
+
+use cpal::{StreamData, UnknownTypeOutputBuffer};
+use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+
+pub fn run() {
+    let host = cpal::default_host();
+    let event_loop = host.event_loop();
+    let device = host.default_output_device().expect("no output device available");
+    let mut supported_formats_range = device.supported_output_formats()
+        .expect("error while querying formats");
+    let format = supported_formats_range.next()
+        .expect("no supported format?!")
+        .with_max_sample_rate();
+    let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
+    event_loop.play_stream(stream_id).expect("failed to play_stream");
+
+    let sample_rate = format.sample_rate.0 as f32;
+    let mut sample_clock = 0f32;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut next_value = || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+    };
+
+    event_loop.run(move |stream_id, stream_result| {
+        let stream_data = match stream_result {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
+                return;
+            }
+            _ => return,
+        };
+
+        match stream_data {
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = (next_value() * std::i16::MAX as f32) as i16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = next_value();
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+            _ => (),
+        }
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use cursive::views::TextView;
 use cursive::Cursive;
 
-mod synthtest;
+mod beeptest;
 
 fn main() {
     let mut siv = Cursive::default();
@@ -11,16 +11,16 @@ fn main() {
     // We can quit by pressing `q`
     siv.add_global_callback('q', Cursive::quit);
 
-    // Test synth by pressing `s`
+    // Test sound by pressing `s`
     siv.add_global_callback('s', |_c| {
-        synthtest::run().unwrap();
+        beeptest::run();
     });
 
     // Add a simple view
     siv.add_layer(TextView::new(
         "Hello, world!\n\
          Press q to quit the application.\n\
-	 Press s to test the synth.",
+	 Press s to test sound.",
     ));
 
     // Run the event loop
